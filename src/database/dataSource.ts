ed4e7fce4d62effef2117c9183f362c078b18241
Arsenii Yurchenko <arseniiyurchenko@Arseniis-MacBook-Air.local>
2024-12-09 13:39:46 +0200

    Fix creds

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'boringly-popular-thrasher.data-1.use1.tembo.io',
-  port: 5432,
-  username: 'postgres',
-  password: 'qsFNxZbOMJqy3N6p',
-  database: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   ssl: {
     rejectUnauthorized: false,
   },
