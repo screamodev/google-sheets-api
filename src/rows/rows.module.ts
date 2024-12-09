3119c50fe2dcf4753541d80d5d6a66d2d893746f
Arsenii Yurchenko <arseniiyurchenko@Arseniis-MacBook-Air.local>
2024-12-09 11:46:40 +0200

    Add missed dep

--- src/rows/rows.module.ts
+++ src/rows/rows.module.ts
@@ -4,10 +4,11 @@ import { RowsController } from './rows.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Row } from './entities/row.entity';
 import { NotificationGateway } from '../notifications/notification.gateway';
+import { EmailService } from '../email/email.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Row])],
   controllers: [RowsController],
-  providers: [RowsService, NotificationGateway],
+  providers: [RowsService, NotificationGateway, EmailService],
 })
 export class RowsModule {}
